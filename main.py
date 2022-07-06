fbd3d038c9161a2aecad5071abb7fd6d21bcc62c
Gilchrist <67920312+Agentgilspy@users.noreply.github.com>
2022-07-06 20:29:40 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,20 @@
 #Importing modules
 
 import os
-from tabulate import tabulate
-from tkinter import *
-from startup import *
+from database import *
+from userpanel import *
 
-from datetime import datetime
-from random import randint
-checkin_d = datetime.strptime("2005/07/23", '%Y/%m/%d') # Get the date from user input
-checkout_d= datetime.strptime("2005/07/23", '%Y/%m/%d') 
+startup()
+while True:
+    os.system('cls')
+    print('\n\n')
+    print('Welcome to Hotel Name')
+    print("""
+1)Staff
+2)User
+""")
+    ch=int(input('Enter choice:'))
+    os.system('cls')
+    if ch==2:
 
-
-
-guestid = 10000 + randint(0, 9999)
-
-
-def reservation():
-    cs.execute('select* from packages')
-    r=cs.fetchall()
-    print(tabulate(r , headers=['PkCode' , "People" , 'Room Type' , 'Package Details', 'Cost Per Night' , 'Tourism']))
-
-    pkchoice=int(input("Select the package:"))
-    cs.execute("select* from packages where pk_code=%s"%(pkchoice))
-    r=cs.fetchall()
-   
-
-
-reservation()
+        userpanel()
