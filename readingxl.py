2e3a3380b161c71dd54264a83b65bb13977c8c6a
Gilchrist <67920312+Agentgilspy@users.noreply.github.com>
2022-07-06 20:29:54 +0400

    Update readingxl.py

--- readingxl.py
+++ readingxl.py
@@ -7,22 +7,13 @@ loc = ("Hotel.xls")
  
 # To open Workbook
 wb = xlrd.open_workbook(loc)
-sheet = wb.sheet_by_index(2)
+sheet = wb.sheet_by_index(1)
  
-rows=range(6,10+1)
+rows=range(1,25)
 
 for i in rows:
-   checkin=sheet.cell_value(i,4)
-   checkin=checkin[1:-1]
-   checkout=sheet.cell_value(i,5)
-   checkout=checkout[1:-1]
-
-
-   checkin= checkin[6:10] + checkin[2:-4] + checkin[0:2]
-   checkout= checkout[6:10] + checkout[2:-4] + checkout[0:2]
-
-   print(f"""cs.execute('insert into Reservations values({int(sheet.cell_value(i,0))},{int(sheet.cell_value(i,1))},{int(sheet.cell_value(i,2))},"0{int(sheet.cell_value(i,3))}","{checkin}","{checkout}",{int(sheet.cell_value(i,6))},{sheet.cell_value(i,7)},{int(sheet.cell_value(i,8))},{int(sheet.cell_value(i,9))})')"""
-   
+   print(f"""cs.execute('insert into Rooms values({int(sheet.cell_value(i,0))},{int(sheet.cell_value(i,1))},"{sheet.cell_value(i,2)}" ,"{sheet.cell_value(i,3)}",Null)')"""
    )
+   
   
 
