ab0028dde01caaad500c546527731dd4e79534b7
Gilchrist <67920312+Agentgilspy@users.noreply.github.com>
2022-12-20 18:00:28 +0400

    Added Password Request feature

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import os
 from database import *
 from userpanel import *
 from staffpanel import *
+import maskpass
+import time
 
 startup()
 while True:
@@ -14,6 +16,11 @@ while True:
     ch=int(input('Enter choice:'))
     os.system('cls')
     if ch==1:
-        staffpanel()
+        pwd=maskpass.askpass('Enter Password:')
+        if pwd=='123':
+            staffpanel()
+        else:
+            print('Wrong Password')
+            time.sleep(2)
     elif ch==2:
         userpanel()
